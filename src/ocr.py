69af2e075629f82403b82b3d0026b37425a01e5c
Nguyen Thien Quang <thienquang12112006@gmail.com>
2026-07-27 10:36:14 +0700

    add filter and save result to seperate files

--- src/ocr.py
+++ src/ocr.py
@@ -1,6 +1,22 @@
 from paddleocr import PaddleOCR
 from pathlib import Path
 
+from dataclasses import dataclass
+import numpy as np
+
+# class object for each text boxes
+
+
+@dataclass
+class OCRResult:
+    text: str
+    poly: np.ndarray
+    score: float
+    box: np.ndarray
+
+
+# instantiate the model
+
 model = PaddleOCR(
     use_doc_orientation_classify=False,
     use_doc_unwarping=False,
@@ -8,11 +24,28 @@ model = PaddleOCR(
     lang="japan",
 )
 
+# specify image path
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 image_path = PROJECT_ROOT / "images" / "image1.jpg"
 
+# inference, OCR the image
 result = model.predict(str(image_path))
 
-res = result[0]  # acces first element 'res'
-for text in res["rec_texts"]:  # res is a dictionary, acces element by key
-    print(text)
+res = result[0]  # acces first element 'res'\
+
+# zip result into list of OCRResult
+format_result = [
+    OCRResult(text, poly, score, box)
+    for text, poly, score, box in zip(
+        res["rec_texts"], res["rec_polys"], res["rec_scores"], res["rec_boxes"]
+    )
+]
+
+with open("output/filtered_result.txt", "w+") as f:
+    for ocrResult in format_result:
+        if ocrResult.score > 0.8:
+            f.write(ocrResult.text + "\n")
+        else:
+            print(ocrResult.text + "|")
+            print(ocrResult.score)
+            print("\n")
