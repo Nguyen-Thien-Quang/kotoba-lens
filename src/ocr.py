320b3dd956f6abe6bc02011e8de71187b978c69b
Nguyen Thien Quang <thienquang12112006@gmail.com>
2026-07-27 22:24:11 +0700

    scratch code to group text boxes into same bubble speech

--- src/ocr.py
+++ src/ocr.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from dataclasses import dataclass
 import numpy as np
+from paddlex.inference.common.batch_sampler import text_batch_sampler
+from paddlex.inference.common.result.converter import build_word_blocks
 
 # class object for each text boxes
 
@@ -41,11 +43,56 @@ format_result = [
     )
 ]
 
-with open("output/filtered_result.txt", "w+") as f:
-    for ocrResult in format_result:
-        if ocrResult.score > 0.8:
-            f.write(ocrResult.text + "\n")
+# delete all furigana
+format_result = [r for r in format_result if r.score >= 0.8]
+# sorting in order of box's position from right to left
+format_result.sort(key=lambda r: r.box[2], reverse=True)
+
+# calculate average text height
+avrg_text_h = 0
+for result in format_result:
+    avrg_text_h = avrg_text_h + result.box[2] - result.box[0]
+
+avrg_text_h = avrg_text_h / len(format_result)
+
+# for res in format_result:
+#     print(res.box[0], res.box[1], res.box[2], res.box[3])
+#     print(res.text)
+# # groups nearby text into clusters
+bubble_id = [-1] * len(format_result)  # list to store bubble_id of each text box
+bubble_speech = list()  # list of bubble text
+thres = avrg_text_h * 0.2  # threshold of distance decide grouping text box
+for i in range(len(format_result)):
+    # first text auto have it own bubble_speech
+    if i == 0:
+        bubble_speech.append(format_result[i].text)  # append as new bubble
+        bubble_id[i] = len(bubble_speech) - 1  # store bubble_id
+        continue
+
+    found_flag = False
+    for j in range(i - 1, -1, -1):  # compare with all text box before
+        horizontal_dis = max(0, format_result[j].box[0] - format_result[i].box[2])
+
+        # stop if meet text box that is already too far from current text box
+        if horizontal_dis > avrg_text_h:
+            break
         else:
-            print(ocrResult.text + "|")
-            print(ocrResult.score)
-            print("\n")
+            # check if vertical position suitable
+            if (
+                format_result[i].box[3] > format_result[j].box[1]
+                and format_result[j].box[3] > format_result[i].box[1]
+            ):
+                # if match: put text[i] into bubble of text[j]
+                bubble_speech[bubble_id[j]] += format_result[i].text
+                bubble_id[i] = bubble_id[j]
+                found_flag = True
+                break
+    if found_flag == True:
+        continue
+    else:
+        bubble_speech.append(format_result[i].text)
+        bubble_id[i] = len(bubble_speech) - 1
+
+print(len(bubble_speech))
+for text in bubble_speech:
+    print(text)
