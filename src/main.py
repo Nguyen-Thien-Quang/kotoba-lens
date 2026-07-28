8bddf736f2f6ed2d5cbcb664c9aaa01888ffe0a6
Nguyen Thien Quang <thienquang12112006@gmail.com>
2026-07-28 20:28:46 +0700

    refactor ocr.py and main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
-# Verify that PaddleOCR is installed successfully
-import paddleocr
+from pathlib import Path
 
-print(f"PaddleOCR version: {paddleocr.__version__}")
+from paddleocr import PaddleOCR
+from paddlex.inference.common.batch_sampler import text_batch_sampler
+from paddlex.inference.common.result.converter import build_word_blocks
 
-# If you use the local paddle_static inference engine, you can further verify PaddlePaddle and GPU availability
-import paddle
+import ocr
 
-print(f"Paddle version: {paddle.__version__}")
-print(f"GPU available: {paddle.is_compiled_with_cuda()}")
-print(f"GPU count: {paddle.device.cuda.device_count()}")
+# instantiate the model
+model = PaddleOCR(
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False,
+    lang="japan",
+)
+
+# specify image_path
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+image_path = PROJECT_ROOT / "images" / "image1.jpg"
+
+# extract text from image,output list of object contain text, bouding boxes and score
+text_boxes = ocr.extract_text(image_path, model)
+# groups text box from same bubble speech into ones
+lines = ocr.clean_text(text_boxes)
+
+for text in lines:
+    print(text)
--- src/ocr.py
+++ src/ocr.py
@@ -1,14 +1,13 @@
-from paddleocr import PaddleOCR
 from pathlib import Path
-
 from dataclasses import dataclass
 import numpy as np
+
+from paddleocr import OCROptions, PaddleOCR
 from paddlex.inference.common.batch_sampler import text_batch_sampler
 from paddlex.inference.common.result.converter import build_word_blocks
 
+
 # class object for each text boxes
-
-
 @dataclass
 class OCRResult:
     text: str
@@ -17,82 +16,71 @@ class OCRResult:
     box: np.ndarray
 
 
-# instantiate the model
+# call model inference and return list of OCRResult object
+def extract_text(image_path, model) -> list[OCRResult]:
+    result = model.predict(str(image_path))
+    data = result[0]  # get the raw_data in 'res' key
+    # format raw_result into text boxes object
+    format_result = [
+        OCRResult(text, poly, score, box)
+        for text, poly, score, box in zip(
+            data["rec_texts"], data["rec_polys"], data["rec_scores"], data["rec_boxes"]
+        )
+    ]
+    return format_result
 
-model = PaddleOCR(
-    use_doc_orientation_classify=False,
-    use_doc_unwarping=False,
-    use_textline_orientation=False,
-    lang="japan",
-)
 
-# specify image path
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
-image_path = PROJECT_ROOT / "images" / "image1.jpg"
+# clean raw ouput from ocr model into list of meaningful, complete text lines
+def clean_text(data) -> list[str]:
+    # data is list of OCRResult object
+    # sort the list of text in order of right to left
+    data.sort(key=lambda r: r.box[2], reverse=True)
 
-# inference, OCR the image
-result = model.predict(str(image_path))
+    # remove furigana
+    data = remove_furigana(data)
 
-res = result[0]  # acces first element 'res'\
+    # calculate average text height
+    avg_text_h = 0
+    for result in data:
+        avg_text_h += result.box[2] - result.box[0]
 
-# zip result into list of OCRResult
-format_result = [
-    OCRResult(text, poly, score, box)
-    for text, poly, score, box in zip(
-        res["rec_texts"], res["rec_polys"], res["rec_scores"], res["rec_boxes"]
-    )
-]
+    avg_text_h = avg_text_h / len(data)
 
-# delete all furigana
-format_result = [r for r in format_result if r.score >= 0.8]
-# sorting in order of box's position from right to left
-format_result.sort(key=lambda r: r.box[2], reverse=True)
+    # groups close OCRResults into same bubble_speech
+    bubble_id = [-1] * len(data)  # list to store bubble_id of each text box
+    bubble_speech = list()  # list of bubble text
+    thres = avg_text_h * 0.2  # threshold of distance decide grouping text box
+    for i in range(len(data)):
+        # first text auto have it own bubble_speech
+        if i == 0:
+            bubble_speech.append(data[i].text)  # append as new bubble
+            bubble_id[i] = len(bubble_speech) - 1  # store bubble_id
+            continue
 
-# calculate average text height
-avrg_text_h = 0
-for result in format_result:
-    avrg_text_h = avrg_text_h + result.box[2] - result.box[0]
+        found_flag = False
+        for j in range(i - 1, -1, -1):  # compare with all text box before
+            horizontal_dis = max(0, data[j].box[0] - data[i].box[2])
 
-avrg_text_h = avrg_text_h / len(format_result)
-
-# for res in format_result:
-#     print(res.box[0], res.box[1], res.box[2], res.box[3])
-#     print(res.text)
-# # groups nearby text into clusters
-bubble_id = [-1] * len(format_result)  # list to store bubble_id of each text box
-bubble_speech = list()  # list of bubble text
-thres = avrg_text_h * 0.2  # threshold of distance decide grouping text box
-for i in range(len(format_result)):
-    # first text auto have it own bubble_speech
-    if i == 0:
-        bubble_speech.append(format_result[i].text)  # append as new bubble
-        bubble_id[i] = len(bubble_speech) - 1  # store bubble_id
-        continue
-
-    found_flag = False
-    for j in range(i - 1, -1, -1):  # compare with all text box before
-        horizontal_dis = max(0, format_result[j].box[0] - format_result[i].box[2])
-
-        # stop if meet text box that is already too far from current text box
-        if horizontal_dis > avrg_text_h:
-            break
-        else:
-            # check if vertical position suitable
-            if (
-                format_result[i].box[3] > format_result[j].box[1]
-                and format_result[j].box[3] > format_result[i].box[1]
-            ):
-                # if match: put text[i] into bubble of text[j]
-                bubble_speech[bubble_id[j]] += format_result[i].text
-                bubble_id[i] = bubble_id[j]
-                found_flag = True
+            # stop if meet text box that is already too far from current text box
+            if horizontal_dis > avg_text_h:
                 break
-    if found_flag == True:
-        continue
-    else:
-        bubble_speech.append(format_result[i].text)
-        bubble_id[i] = len(bubble_speech) - 1
+            else:
+                # check if vertical position suitable
+                if data[i].box[3] > data[j].box[1] and data[j].box[3] > data[i].box[1]:
+                    # if match: put text[i] into bubble of text[j]
+                    bubble_speech[bubble_id[j]] += data[i].text
+                    bubble_id[i] = bubble_id[j]
+                    found_flag = True
+                    break
+        if found_flag == True:
+            continue
+        else:
+            bubble_speech.append(data[i].text)
+            bubble_id[i] = len(bubble_speech) - 1
+    return bubble_speech
 
-print(len(bubble_speech))
-for text in bubble_speech:
-    print(text)
+
+def remove_furigana(data) -> list[OCRResult]:
+    # remove furigana by remove all text with confidence score smaller than 0.8
+    data = [r for r in data if r.score >= 0.8]
+    return data
